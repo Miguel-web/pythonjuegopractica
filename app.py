eff127aea355342833972cfcdd812a3e07a9e2df
Miguel-web <miguelgilf@gmail.com>
2023-04-25 21:46:46 +0000

    worshop mas ejercicios

--- app.py
+++ app.py
@@ -1,5 +1,52 @@
 name = "Miguel"
 age = 37
-box = ["Hola"]
+lastname ="Gil"
 
-print (name)
+# box = ["Hola"]
+
+# imprime el nombre o la variable
+#print (name)
+
+# print (type(age))
+
+# tipos de datos: 
+# 1- Enteros int()
+# 2- decimal float()
+# 3- String str()
+# 4- Enteros Bool()
+
+# operadores logicos : 
+# and : ambas tienen que ser verdaderas
+# or , 
+# not
+
+# condicionales
+# if 
+# else
+
+# if name == "Miguel" and lastname == "Gil":
+#     print("Verdadero")
+
+# if name == "Miguel" or lastname == "Gil":
+#     print("Verdadero")
+
+# if age < 18:
+#     print("Puede ver la pelicula")
+# else:
+#     print("No puede ver la pelicula")
+
+# loop
+lista = [Miguel, 16, True]
+
+counter = 0
+
+while True:
+    print(counter)
+counter = counter +1    
+if counter >= 10:
+    break
+
+for cosas in lista:
+    print(cosas)
+
+print("*" * 20)
